95d2df219aaa3be614f271d71714aa7fcda78faf
Al Hadi <alhadi1009.bd@gmail.com>
2025-12-29 22:04:22 +0600

    add Effects

--- Plane.game/src/game/component/PanelGame.java
+++ Plane.game/src/game/component/PanelGame.java
@@ -1,6 +1,7 @@
-
 package game.component;
+
 import game.obj.Bullet;
+import game.obj.Effect;
 import game.obj.Player;
 import static game.obj.Player.PLAYER_SIZE;
 import game.obj.Rocket;
@@ -22,311 +23,282 @@ import static javax.swing.DropMode.ON;
 
 import javax.swing.JComponent;
 
-
-public class PanelGame extends JComponent{
+public class PanelGame extends JComponent {
 
     private Graphics2D g2;
     private BufferedImage image;
     private int width;
     private int height;
     private Thread thread;
-    private boolean start=true;
+    private boolean start = true;
     private Key key;
     private int shotTime;
-    
-    
+
 //Game FPS
-    private final int FPS=60;
-    private final int TARGET_TIME=1000000000 / FPS;
+    private final int FPS = 60;
+    private final int TARGET_TIME = 1000000000 / FPS;
     // Game Object
     private Player player;
-    private List<Bullet>bullets;
-    private List<Rocket>rockets;
-    
-    public void start()
-    {
+    private List<Bullet> bullets;
+    private List<Rocket> rockets;
+    private List<Effect> boomEffects;
+
+    public void start() {
         width = getWidth();
         height = getHeight();
-        image=new BufferedImage(width, height, BufferedImage.TYPE_INT_ARGB);
-        g2=image.createGraphics();
-        g2.setRenderingHint(RenderingHints.KEY_ANTIALIASING,RenderingHints.VALUE_ANTIALIAS_ON);
-        g2.setRenderingHint(RenderingHints.KEY_INTERPOLATION,RenderingHints.VALUE_INTERPOLATION_BILINEAR);
-        
-        thread = new Thread(new Runable(){
+        image = new BufferedImage(width, height, BufferedImage.TYPE_INT_ARGB);
+        g2 = image.createGraphics();
+        g2.setRenderingHint(RenderingHints.KEY_ANTIALIASING, RenderingHints.VALUE_ANTIALIAS_ON);
+        g2.setRenderingHint(RenderingHints.KEY_INTERPOLATION, RenderingHints.VALUE_INTERPOLATION_BILINEAR);
+
+        thread = new Thread(new Runable() {
             @Override
-            public void run()
-            {
-                while(start)
-                {
-                    long startTime=System.nanoTime();
+            public void run() {
+                while (start) {
+                    long startTime = System.nanoTime();
                     updateBullets();
                     drawBackground();
                     drawGame();
                     render();
-                    long time=System.nanoTime()-startTime;
-                    if(time<TARGET_TIME)
-                    {
-                        long sleep=(TARGET_TIME-time)/1000000;
-                         sleep(sleep);
-                       // System.out.println(sleep);
+                    long time = System.nanoTime() - startTime;
+                    if (time < TARGET_TIME) {
+                        long sleep = (TARGET_TIME - time) / 1000000;
+                        sleep(sleep);
+                        // System.out.println(sleep);
                     }
-                   // sleep(TARGET_TIME);//FOR TEST
+                    // sleep(TARGET_TIME);//FOR TEST
                 }
             }
-            
+
         });
         initObjectGame();
         initBullets();      // <<< BEFORE threads start
         initKeyboard();
         thread.start();
     }
-    
-    private void addRocket()
-    {
-        Random ran=new Random();
-        int locationY=ran.nextInt(height-50)+25;
-        Rocket rocket=new Rocket();
-        rocket.changeLocation(0,locationY);
+
+    private void addRocket() {
+        Random ran = new Random();
+        int locationY = ran.nextInt(height - 50) + 25;
+        Rocket rocket = new Rocket();
+        rocket.changeLocation(0, locationY);
         rocket.changeAngle(0);
         rockets.add(rocket);
-        int locationY2=ran.nextInt(height-50)+25;
-        Rocket rocket2=new Rocket();
+        int locationY2 = ran.nextInt(height - 50) + 25;
+        Rocket rocket2 = new Rocket();
         rocket2.changeLocation(width, locationY2);
         rocket2.changeAngle(180);
         rockets.add(rocket2);
 
-        
-        
     }
-    
-    private void initObjectGame(){
-        player=new Player();
-       
-        player.changeLocation(150,150);
+
+    private void initObjectGame() {
+        player = new Player();
+
+        player.changeLocation(150, 150);
         rockets = new ArrayList<>();
-        new Thread(new Runnable()
-        {
+        boomEffects = new ArrayList<>();
+        new Thread(new Runnable() {
             @Override
-            public void run(){
-                while(start)
-                {
+            public void run() {
+                while (start) {
                     addRocket();
                     sleep(3000);
                 }
-                
+
             }
         }).start();
     }
-    private void initKeyboard()
-    {
-        key=new Key();
+
+    private void initKeyboard() {
+        key = new Key();
         requestFocus();
-        addKeyListener(new KeyAdapter()
-        {
+        addKeyListener(new KeyAdapter() {
             @Override
-            public void keyPressed(KeyEvent e)
-            {
-                if(e.getKeyCode()==KeyEvent.VK_A)
-                {
+            public void keyPressed(KeyEvent e) {
+                if (e.getKeyCode() == KeyEvent.VK_A) {
                     key.setKey_left(true);
-                }
-                else if(e.getKeyCode()==KeyEvent.VK_D)
-                {
-                  key.setKey_right(true);   
-                }
-                else if(e.getKeyCode()==KeyEvent.VK_SPACE)
-                {
-                     key.setKey_space(true);
-                }
-                else if(e.getKeyCode()==KeyEvent.VK_J)
-                {
+                } else if (e.getKeyCode() == KeyEvent.VK_D) {
+                    key.setKey_right(true);
+                } else if (e.getKeyCode() == KeyEvent.VK_SPACE) {
+                    key.setKey_space(true);
+                } else if (e.getKeyCode() == KeyEvent.VK_J) {
                     key.setKey_j(true);
-                }else if(e.getKeyCode()==KeyEvent.VK_K)
-                {
+                } else if (e.getKeyCode() == KeyEvent.VK_K) {
                     key.setKey_k(true);
                 }
             }
+
             @Override
-            public void keyReleased(KeyEvent e)
-            {
-                 if(e.getKeyCode()==KeyEvent.VK_A)
-                {
+            public void keyReleased(KeyEvent e) {
+                if (e.getKeyCode() == KeyEvent.VK_A) {
                     key.setKey_left(false);
-                }
-                else if(e.getKeyCode()==KeyEvent.VK_D)
-                {
-                  key.setKey_right(false);   
-                }
-                else if(e.getKeyCode()==KeyEvent.VK_SPACE)
-                {
-                     key.setKey_space(false);
-                }
-                else if(e.getKeyCode()==KeyEvent.VK_J)
-                {
+                } else if (e.getKeyCode() == KeyEvent.VK_D) {
+                    key.setKey_right(false);
+                } else if (e.getKeyCode() == KeyEvent.VK_SPACE) {
+                    key.setKey_space(false);
+                } else if (e.getKeyCode() == KeyEvent.VK_J) {
                     key.setKey_j(false);
-                }else if(e.getKeyCode()==KeyEvent.VK_K)
-                {
+                } else if (e.getKeyCode() == KeyEvent.VK_K) {
                     key.setKey_k(false);
                 }
-                
+
             }
         });
-        new Thread (new Runnable(){
+        new Thread(new Runnable() {
             @Override
-            public void run()
-            {
-                float s=0.5f;
-                while(start)
-                {
-                    float angle =player.getAngle();
-                    if(key.isKey_left())
-                    {
-                        angle-=s;
+            public void run() {
+                float s = 0.5f;
+                while (start) {
+                    float angle = player.getAngle();
+                    if (key.isKey_left()) {
+                        angle -= s;
                     }
-                    if(key.isKey_right())
-                    {
-                        angle+=s;
+                    if (key.isKey_right()) {
+                        angle += s;
                     }
-                    if(key.isKey_j() || key.isKey_k())
-                    {
-                        if(shotTime==0)
-                        {
-                            if(key.isKey_j())
-                            {
-                                bullets.add(0,new Bullet(player.getX(),player.getY(),player.getAngle(), 5, 3f));
-                            }else
-                            {
-                                bullets.add(0,new Bullet(player.getX(),player.getY(),player.getAngle(), 12, 3f));
+                    if (key.isKey_j() || key.isKey_k()) {
+                        if (shotTime == 0) {
+                            if (key.isKey_j()) {
+                                bullets.add(0, new Bullet(player.getX(), player.getY(), player.getAngle(), 5, 3f));
+                            } else {
+                                bullets.add(0, new Bullet(player.getX(), player.getY(), player.getAngle(), 12, 3f));
 
                             }
-                           
+
                         }
-                         shotTime++;
-                            if(shotTime==25)
-                            {
-                                shotTime=0;
-                            }
-                    }else
-                    {
-                        shotTime=0 ;
+                        shotTime++;
+                        if (shotTime == 25) {
+                            shotTime = 0;
+                        }
+                    } else {
+                        shotTime = 0;
                     }
-                    
-                    
-                    if(key.isKey_space())
-                    {
+
+                    if (key.isKey_space()) {
                         player.speedUp();
-                    }else 
-                    {
+                    } else {
                         player.speedDown();
                     }
                     player.update();
                     player.changeAngle(angle);
-                   for(int i=0;i<rockets.size();i++)
-                   {
-                       Rocket rocket= rockets.get(i);
-                       if(rocket!=null)
-                       {
-                           rocket.update();
-                           if(!rocket.check(width,height))
-                           {
-                               rockets.remove(rocket);
-                               System.out.println("Removed ...");
-                           }
-                           
-                       }
-                   }
-                    
-                    
+                    for (int i = 0; i < rockets.size(); i++) {
+                        Rocket rocket = rockets.get(i);
+                        if (rocket != null) {
+                            rocket.update();
+                            if (!rocket.check(width, height)) {
+                                rockets.remove(rocket);
+                                System.out.println("Removed ...");
+                            }
+
+                        }
+                    }
+
                     sleep(5);
-                    
+
                 }
             }
-            
-        }).start(); 
-      
-        
-    }
-    
-   private void initBullets()
-    {
-        bullets =new ArrayList<>();
-    }
-    private void updateBullets() {
-    for(int i=0; i<bullets.size(); i++){
-        Bullet bullet = bullets.get(i);
-        bullet.update();
-         checkBullets( bullet);
-        if(!bullet.check(width, height)){
-            bullets.remove(i);
-            i--; 
-        }
-    }
-}
 
-public void checkBullets(Bullet bullet)
-{
-    for(int i=0;i<rockets.size();i++)
-    {
-        Rocket rocket=rockets.get(i);
-        if(rocket!=null)
-        {
-            Area area=new Area(bullet.getShape());
-            area.intersect(rocket.getShape());
-            if(!area.isEmpty())
-            {
-                rockets.remove(rocket);
-                bullets.remove(bullet);
+        }).start();
+
+    }
+
+    private void initBullets() {
+        bullets = new ArrayList<>();
+    }
+
+    private void updateBullets() {
+        for (int i = 0; i < bullets.size(); i++) {
+            Bullet bullet = bullets.get(i);
+            bullet.update();
+            checkBullets(bullet);
+            if (!bullet.check(width, height)) {
+                bullets.remove(i);
+                i--;
+            }
+
+        }
+        for (int i = 0; i < boomEffects.size(); i++) {
+            Effect boomEffect = boomEffects.get(i);
+            if (boomEffect != null) {
+                boomEffect.update();
+                if (!boomEffect.check()) {
+                    boomEffects.remove(boomEffect);
+                }
+            } else {
+                boomEffects.remove(boomEffect);
+            }
+        }
+
+    }
+
+    public void checkBullets(Bullet bullet) {
+        for (int i = 0; i < rockets.size(); i++) {
+            Rocket rocket = rockets.get(i);
+            if (rocket != null) {
+                Area area = new Area(bullet.getShape());
+                area.intersect(rocket.getShape());
+                if (!area.isEmpty()) {
+                    boomEffects.add(new Effect(bullet.getCenterX(), bullet.getCenterY(), 3, 5, 60, 0.5f, new Color(230, 207, 105)));
+                    if (true) {
+                        rockets.remove(rocket);
+                        double x = rocket.getX() + Rocket.ROCKET_SIZE / 2;
+                        double y = rocket.getY() + Rocket.ROCKET_SIZE / 2;
+                        boomEffects.add(new Effect(x, y, 5, 5, 75, 0.05f, new Color(32, 178, 169)));
+
+                    }
+                    // rockets.remove(rocket);
+                    bullets.remove(bullet);
+                    return;
+                }
             }
         }
     }
-}
-    
-    private void drawBackground()
-    {
-        g2.setColor(new Color(30,30,30));
-        g2.fillRect(0,0,width,height);
+
+    private void drawBackground() {
+        g2.setColor(new Color(30, 30, 30));
+        g2.fillRect(0, 0, width, height);
     }
-    private void drawGame()
-    {
+
+    private void drawGame() {
         player.draw(g2);
-        for(int i=0;i<bullets.size();i++)
-        {
+        for (int i = 0; i < bullets.size(); i++) {
             Bullet bullet = bullets.get(i);
-            if(bullet != null)
-            {
+            if (bullet != null) {
                 bullet.draw(g2);
             }
-        }  
-        for(int i=0;i<rockets.size();i++)
-        {
+        }
+        for (int i = 0; i < rockets.size(); i++) {
             Rocket rocket = rockets.get(i);
-            if(rocket !=null)
-            {
+            if (rocket != null) {
                 rocket.draw(g2);
             }
         }
-        
-        
-        
-        
+        for (int i = 0; i < boomEffects.size(); i++) {
+            Effect boomEffect = boomEffects.get(i);
+            if (boomEffect != null) {
+                boomEffect.draw(g2);
+            }
+        }
+
     }
-    private void render()
-    {
+
+    private void render() {
         Graphics g = getGraphics();
-        g.drawImage(image,0,0,null);
+        g.drawImage(image, 0, 0, null);
         g.dispose();
-        
+
     }
-    private void sleep(long speed){
-        try{
+
+    private void sleep(long speed) {
+        try {
             Thread.sleep(speed);
-            
-        }catch (InterruptedException ex)
-        {
+
+        } catch (InterruptedException ex) {
             // Logger.getLogger(PanelGame.class.getName()).log(Level.SEVERE, null, ex);
             System.err.println(ex);
         }
-        
+
     }
 
 }
